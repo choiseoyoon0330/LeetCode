3db556ff8496e94f9ca466e3e498b1f58fc67aed
Seoyoon Choi <144862626+choiseoyoon0330@users.noreply.github.com>
2025-03-01 15:18:56 +0900

    Time: 0 ms (100%), Space: 18.9 MB (34.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            complement  = target - num
+
+            if complement in num_map:
+                return [num_map[complement], i]
+
+            num_map[num] = i
+
+        return []
+
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))
